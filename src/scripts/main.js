622a53f93f820ce70218b718a1728a12d903924d
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-08-02 11:28:40 -0500

    Created pottery-wheel module to include function to create pottery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
 // Imports go first
-
-
+import { makePottery } from './pottery-wheel.js';
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
 
+let mug = makePottery('Mug', 1, 3);
+let platter = makePottery('Platter', 5, 1);
+let vase = makePottery('Vase', 2, 10);
+let bowl = makePottery('Bowl', 3, 4);
+let plate = makePottery('Plate', 2, 2);
 
 // Fire each piece of pottery in the kiln
 
-
 // Determine which ones should be sold, and their price
 
-
 // Invoke the component function that renders the HTML list
-
